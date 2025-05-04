2fcda3e7022eb4e3cf3edc7386639037e4fe23ad
Pedro4064 <pedrohlcruz@gmail.com>
2025-05-04 15:16:23 -0300

    [IMPLEMENTED] Testcase for example given

--- tests/test_max_area.c
+++ tests/test_max_area.c
@@ -3,25 +3,33 @@
 
 #include "max_area.h"
 
-void setUp(void){
+void setUp(void) {}
 
+void tearDown(void) {}
+
+static void test_empty_map(void) {
+  char map[3][4] = {
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},
+      {0, 0, 0, 0},
+  };
+  TEST_ASSERT_EQUAL(0, calculate_max_area((char *)map, 3, 4));
 }
 
-void tearDown(void){
+static void test_example_given(void) {
+  char map[4][5] = {
+    {1, 0, 1, 0, 0},
+    {1, 0, 1, 1, 1},
+    {1, 1, 1, 1, 1},
+    {1, 0, 0, 1, 0}
+  };
 
+  TEST_ASSERT_EQUAL(6, calculate_max_area((char *)map, 4, 5));
 }
 
-static void test_empty_map(void){
-    char map[3][4] = {
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-    };
-    TEST_ASSERT_EQUAL(0, calculate_max_area((char *)map, 3, 4));
-}
-
-int main(void){
-    UNITY_BEGIN();
-    RUN_TEST(test_empty_map);
-    return UNITY_END();
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_empty_map);
+  RUN_TEST(test_example_given);
+  return UNITY_END();
 }
