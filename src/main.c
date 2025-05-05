31fdb3fc5b5bf1fad559d38ac3888fe29bc22437
Pedro4064 <pedrohlcruz@gmail.com>
2025-05-05 17:52:47 -0300

    [ADDED] Configs to debug

--- src/main.c
+++ src/main.c
@@ -3,14 +3,14 @@
 
 int main(void) {
   printf("Running...\n");
-  char map[4][5] = {
-    {1, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0},
-    {1, 0, 0, 0, 0}
+
+  char map[5][6] = {
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 1, 1, 1, 1, 1},
+      {0, 1, 1, 0, 1, 0},
+      {0, 0, 1, 0, 1, 0},
   };
-
-  calculate_max_area((char *)map, 4, 5);
-
+  calculate_max_area((char *)map, 5, 6);
   return 0;
 }
