9d5b07419485190c4de225b9044e89ae757b08fd
Pedro4064 <pedrohlcruz@gmail.com>
2025-05-05 17:04:04 -0300

    [IMPLEMENTED] Additional test cases

--- tests/test_max_area.c
+++ tests/test_max_area.c
@@ -18,18 +18,67 @@ static void test_empty_map(void) {
 
 static void test_example_given(void) {
   char map[4][5] = {
-    {1, 0, 1, 0, 0},
-    {1, 0, 1, 1, 1},
-    {1, 1, 1, 1, 1},
-    {1, 0, 0, 1, 0}
+      {1, 0, 1, 0, 0},
+      {1, 0, 1, 1, 1},
+      {1, 1, 1, 1, 1},
+      {1, 0, 0, 1, 0},
   };
 
   TEST_ASSERT_EQUAL(6, calculate_max_area((char *)map, 4, 5));
+}
+
+static void test_vertical_line(void) {
+  char map[4][5] = {
+      {1, 0, 0, 0, 0},
+      {1, 0, 0, 0, 0},
+      {1, 0, 0, 0, 0},
+      {1, 0, 0, 0, 0},
+  };
+
+  TEST_ASSERT_EQUAL(4, calculate_max_area((char *)map, 4, 5));
+}
+
+static void test_horizontal_line(void) {
+  char map[4][5] = {
+      {1, 1, 1, 1, 1},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0},
+  };
+
+  TEST_ASSERT_EQUAL(5, calculate_max_area((char *)map, 4, 5));
+}
+
+static void test_full(void) {
+  char map[4][5] = {
+      {1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1},
+      {1, 1, 1, 1, 1},
+  };
+
+  TEST_ASSERT_EQUAL(5*4, calculate_max_area((char *)map, 4, 5));
+}
+
+static void test_variation_1(void) {
+  char map[5][6] = {
+      {0, 0, 0, 0, 0, 0},
+      {0, 0, 0, 0, 0, 0},
+      {0, 1, 1, 1, 1, 1},
+      {0, 1, 1, 0, 1, 0},
+      {0, 0, 1, 0, 1, 0},
+  };
+
+  TEST_ASSERT_EQUAL(5, calculate_max_area((char *)map, 5, 6));
 }
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_empty_map);
   RUN_TEST(test_example_given);
+  RUN_TEST(test_vertical_line);
+  RUN_TEST(test_horizontal_line);
+  RUN_TEST(test_full);
+  RUN_TEST(test_variation_1);
   return UNITY_END();
 }
