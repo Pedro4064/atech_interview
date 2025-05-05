ba4c84559f617002681412f198ef1c0233f9295b
Pedro4064 <pedrohlcruz@gmail.com>
2025-05-05 13:22:34 -0300

    [FIXED] function signature considering 2d array decay

--- src/max_area.c
+++ src/max_area.c
@@ -1,3 +1,11 @@
 #include "max_area.h"
 
-int calculate_max_area(char *input_matrix, unsigned int M, unsigned int N) {}
+int calculate_max_area(char **input_matrix, unsigned int M, unsigned int N) {
+  for (int row_index = 0; row_index < M; row_index++) {
+    for (int column_index = 0; column_index < N; column_index++) {
+
+      if (input_matrix[row_index][column_index] == 0)
+        continue;
+    }
+  }
+}
